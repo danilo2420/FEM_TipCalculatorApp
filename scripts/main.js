7176378993936fb4403f14894ee26f8a172263de
dani <danimoraga1997@gmail.com>
2025-07-07 11:57:38 +0200

    Working on calculatiosn

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,31 @@
 // Elements
+const inputBill = document.querySelector('.calculator__input__bill__label__input');
 const buttons = document.querySelectorAll('.calculator__input__tip__select__item');
+const inputPeople = document.querySelector('.calculator__input__people__input');
 
 // Variables
-let chosenTip = undefined;
+let chosenTip = 15;
 
 function main() {
-    // Click on button functionality
-    configureTipButtons();
     // Main calculation functionality
+    setEventListeners();
 
     // Reset functionality
 }
 
-function configureTipButtons() {
+function setEventListeners() {
+    inputBill.addEventListener('input', () => {
+        calculateTip();
+    });
     buttons.forEach(
         (button) => button.addEventListener(
             'click', 
             (event) => handleSelectTipButton(event.target)
         )
     );
+    inputPeople.addEventListener('input', () => {
+        calculateTip();
+    })
 }
 
 const handleSelectTipButton = (button) => {
@@ -27,7 +34,42 @@ const handleSelectTipButton = (button) => {
         button.classList.remove('item--active');
     })
     button.classList.add('item--active');
-    console.log('You chose this tip: ' + chosenTip + '%');
+    calculateTip();
 }
+
+function calculateTip() {
+    console.log('Calculating tip...');
+    // Get values
+    const bill = inputBill.value;
+    const people = inputPeople.value;
+
+    // Validation
+    if (!validateInput(bill, people)) {
+        console.log('Input did not pass validation');
+        return;
+    }
+    
+
+    // Set result
+}
+
+function validateInput(bill, people) {
+    if (isNaN(bill) || bill.length == 0) {
+        console.log('returning');
+        return false;
+    }
+
+    if (chosenTip == undefined) {
+        return false;
+    }
+
+    if (isNaN(people) || people.length == 0) {
+        console.log('returning');
+        return false;
+    }
+
+    return true;
+}
+
 
 main();
