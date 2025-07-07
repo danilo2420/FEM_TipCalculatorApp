192e93eb24ba2c836d2d76519b589e21ca8e879d
dani <danimoraga1997@gmail.com>
2025-07-07 12:13:49 +0200

    Working on functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,11 @@
 // Elements
+// # Inputs
 const inputBill = document.querySelector('.calculator__input__bill__label__input');
 const buttons = document.querySelectorAll('.calculator__input__tip__select__item');
 const inputPeople = document.querySelector('.calculator__input__people__input');
+// # Outputs
+const outputTip = document.querySelector('.outputTip');
+const outputTotal = document.querySelector('.outputTotal');
 
 // Variables
 let chosenTip = 15;
@@ -46,11 +50,17 @@ function calculateTip() {
     // Validation
     if (!validateInput(bill, people)) {
         console.log('Input did not pass validation');
+        outputTotal.innerHTML = formatCurrency(0);
+        outputTip.innerHTML = formatCurrency(0);
         return;
     }
-    
 
     // Set result
+    let totalPerPerson = (bill * (1 + (chosenTip/100))) / people;
+    let tipPerPerson = (bill * (chosenTip/100)) / people;
+
+    outputTotal.innerHTML = formatCurrency(totalPerPerson);
+    outputTip.innerHTML = formatCurrency(tipPerPerson);
 }
 
 function validateInput(bill, people) {
@@ -69,6 +79,15 @@ function validateInput(bill, people) {
     }
 
     return true;
+}
+
+function formatCurrency(value) {
+    if (isNaN(value)) return;
+
+    value *= 100;
+    value = Math.floor(value);
+
+    return '$' + (value/100);
 }
 
 
